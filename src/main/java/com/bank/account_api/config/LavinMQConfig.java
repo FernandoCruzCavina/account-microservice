bf25dd0e8d9e44afb445152055af04a8f03b1ea0
Fernando Cruz Cavina <fernando.cruz.cavina@gmail.com>
2025-04-21 22:47:31 -0300

    feat: handle incoming account data reqs via RabbitMQ

---
+++ src/main/java/com/bank/account_api/config/LavinMQConfig.java
@@ -0,0 +1,26 @@
+package com.bank.account_api.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Configuration
+public class LavinMQConfig {
+    @Value("${broker.queue.user.name}")
+    public String queueName;
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queueName, true, false, false);
+    }
+    
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+}
---
+++ src/main/java/com/bank/account_api/consumer/AccountConsumer.java
@@ -0,0 +1,33 @@
+package com.bank.account_api.consumer;
+
+import javax.swing.text.View;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.bank.account_api.dto.ViewAccountDto;
+import com.bank.account_api.service.AccountService;
+
+@Component
+public class AccountConsumer {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+    
+    @Autowired
+    private AccountService accountService;
+    
+    private String routingKey = "payment.replies";
+
+    @RabbitListener(queues = "${broker.queue.user.name}")
+    public void replyAccount(long id) {
+        ViewAccountDto account = accountService.getAccountById(id);
+
+        Message message = rabbitTemplate.getMessageConverter().toMessage(account, null);
+
+        rabbitTemplate.send("", routingKey, message);
+    }
+}
